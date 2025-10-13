36cbf8f953ae371b9f9ae85c7f92b1e859d2857f
jamesjdigitalco <james@j-digitalco.com>
2025-10-13 21:00:42 +0800

    Add index file.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Applications.me ❤️ GHL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<h1>Applications.me ❤️ GHL</h1>
+<p>Code obtained is <?php echo $_GET['code'] ?? 'ERROR_IN_GETTING_CODE' ?></p>
+
+</body>
+</html>
